0a8e679fe810d8c0d7a8baddd1abf6fcedc69a8d
qxb3 <toolong972@gmail.com>
2022-05-04 14:06:24 +0800

    Added MirageCommand (Not fixed)

--- src/lib/MirageCommand.js
+++ src/lib/MirageCommand.js
@@ -1,13 +1,37 @@
 const { Command } = require('@sapphire/framework')
+const { Message, CommandInteraction } = require('discord.js')
 
 class MirageCommand extends Command {
   constructor(context, options) {
-    const { description } = options
-
     super(context, {
       ...options,
     })
+
+    this.commandType = options.commandType
   }
+
+  run(context, args) {
+    if (context instanceof Message) {
+      this.noArgs()
+    }
+
+    if (context instanceof CommandInteraction) {
+      this.noArgs()
+    }
+  }
+
+  async messageRun(message, args) {
+    this.run(message, args.rest().catch(err => console.log(err)))
+  }
+
+  chatInputRun(interaction) {
+    this.run(interaction)
+  }
+
+  noArgs() {}
+  isCategory() {}
+  isItem() {}
+  isError() {}
 }
 
 module.exports = MirageCommand
