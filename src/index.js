ff8beef2f9425c4a42a6c7322423237f71600cc7
qxb3 <toolong972@gmail.com>
2022-05-08 22:23:36 +0800

    using MirageClient

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 require('dotenv/config')
 require('module-alias/register')
 
-const { SapphireClient, ApplicationCommandRegistries, LogLevel } = require('@sapphire/framework')
-const mongodb = require('#src/mongodb')
-
-const client = new SapphireClient({
-  intents: [
-    'GUILDS',
-    'GUILD_MESSAGES',
-    'GUILD_MEMBERS'
-  ],
-  defaultPrefix: '?',
-  logger: { level: process.env.NODE_ENV === 'development' ? LogLevel.Debug : LogLevel.Info },
-  loadMessageCommandListeners: true,
-  caseInsensitiveCommands: true,
-  caseInsensitivePrefixes: true,
-})
-
-ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical('OVERWRITE')
-
-mongodb.connect(client.logger, () => {
-  client.login(process.env.BOT_TOKEN)
-})
+const MirageClient = require('#lib/MirageClient')
+const client = new MirageClient()
+client.login()
