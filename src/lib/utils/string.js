8c5db4b9dcd25926103f68d5fb5bb20c111f9ac4
qxb3 <qxbthree@gmail.com>
2022-05-11 23:02:12 +0800

    feat: added capitalizeAll utility function This function will capitalize the first letter in every word

--- src/lib/utils/string.js
+++ src/lib/utils/string.js
@@ -6,7 +6,15 @@ const ignoreCase = (str1, str2) => {
   return str1.toLowerCase() === str2.toLowerCase()
 }
 
+const capitalizeAll = (str) => {
+  return str.split(' ')
+    .map(v =>
+      v.charAt(0).toUpperCase() + v.substring(1)
+    ).join(' ')
+}
+
 module.exports = {
   addCircleOnFront,
-  ignoreCase
+  ignoreCase,
+  capitalizeAll
 }
