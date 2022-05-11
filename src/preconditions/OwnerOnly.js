c8dc0e127442dc0ecb07beaa66fead73d2b6a33d
qxb3 <qxbthree@gmail.com>
2022-05-12 01:02:24 +0800

    feat: added OwnerOnly precondition

---
+++ src/preconditions/OwnerOnly.js
@@ -0,0 +1,31 @@
+const { Precondition } = require('@sapphire/framework')
+
+class OwnerOnlyPrecondition extends Precondition {
+  messageRun(message) {
+    return this.check(message.author) ?
+      this.ok() :
+      this.error({
+        message: this.getMessage(),
+        context: message
+      })
+  }
+
+  chatInputRun(interaction) {
+    return this.check(interaction.user) ?
+      this.ok() :
+      this.error({
+        message: this.getMessage(),
+        context: interaction
+      })
+  }
+
+  check(user) {
+    return user.id === process.env.DEV_ID + '0'
+  }
+
+  getMessage() {
+    return 'Only the developer can use this command'
+  }
+}
+
+module.exports = OwnerOnlyPrecondition
