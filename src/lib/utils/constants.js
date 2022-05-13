f348c9a9d6187449870c4514ce17b7b765f94438
qxb3 <qxbthree@gmail.com>
2022-05-13 20:04:16 +0800

    feat(constants): change primary and secondary colors

--- src/lib/utils/constants.js
+++ src/lib/utils/constants.js
@@ -1,8 +1,8 @@
 const { Constants } = require('discord.js')
 
 const Colors = {
-  Primary: Constants.Colors.GREEN,
-  Secondary: Constants.Colors.YELLOW,
+  Primary: Constants.Colors.DARK_BUT_NOT_BLACK,
+  Secondary: Constants.Colors.ORANGE,
   Error: Constants.Colors.RED
 }
 
