a2d07e7b9037cbd373aad21809785488dcbbbdc3
qxb3 <qxbthree@gmail.com>
2022-05-15 12:00:53 +0800

    feat: added scrolls command

---
+++ src/commands/Items/scrolls.js
@@ -0,0 +1,38 @@
+const WikiCommand = require('#structures/commands/WikiCommand')
+const { createEmbedUser } = require('#utils/response')
+const { addCircleOnFront } = require('#utils/string')
+
+class ScrollsCommand extends WikiCommand {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      description: 'Find scrolls in the game',
+      aliases: ['scroll'],
+
+      items: 'items/scrolls.json',
+      thumbnail: {
+        name: 'experience-scroll',
+        path: 'assets/items/sprites/scrolls/experience-scroll.png'
+      },
+      commandUsages: [
+        { arg: '[scroll]', description: 'To see the full info of the scroll', example: 'Experience scroll' }
+      ]
+    })
+  }
+
+  getItemResponse({ item, user }) {
+    const spriteName = `${item.full_name.toLowerCase().replace(/ /g, '-').replace(/'/, '')}.png`
+    const sprite = `assets/items/sprites/${this.name}/${spriteName}`
+
+    const embed = createEmbedUser(user)
+      .setThumbnail(`attachment://${spriteName}`)
+      .addField('❯ Name', item.full_name)
+      .addField('❯ Effects', addCircleOnFront(item.effects))
+      .addField('❯ Real Life Prices', addCircleOnFront(item.real_prices))
+      .addField('❯ In-Game Prices', item.ingame_price)
+
+    return { embed, sprite }
+  }
+}
+
+module.exports = ScrollsCommand
