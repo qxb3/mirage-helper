8c197fed458dfdcec6792f93a9bff986122ff381
qxb3 <qxbthree@gmail.com>
2022-05-20 10:50:27 +0800

    feat: adds enchantments command

---
+++ src/commands/Wiki/enchantments.js
@@ -0,0 +1,33 @@
+const WikiCommand = require('#structures/commands/WikiCommand')
+const { createEmbedUser } = require('#utils/response')
+const { addCircleOnFront } = require('#utils/string')
+
+class EnchantmentsCommand extends WikiCommand {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      description: 'Find enchantments in the game',
+      aliases: ['enchantment', 'enchants', 'enchant'],
+
+      thumbnail: 'assets/wiki/sprites/enchantments/physical.png',
+      commandUsages: [
+        { arg: '[enchantment]', description: 'To see the full info of the enchantment', example: 'Mana' },
+      ]
+    })
+  }
+
+  getItemResponse({ item, user }) {
+    const { name, path } = this.getSprite(this, item)
+
+    const embed = createEmbedUser(user)
+      .setThumbnail(`attachment://${name}`)
+      .addField('❯ Name', item.name)
+      .addField('❯ Enchanter', item.enchanter)
+      .addField('❯ Location', item.location)
+      .addField('❯ Materials Required', addCircleOnFront(item.materials_required))
+
+    return { embed, sprite: path }
+  }
+}
+
+module.exports = EnchantmentsCommand
