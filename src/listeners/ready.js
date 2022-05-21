5d8582941ae74b07fd9aecd69df6af309178c9a1
qxb3 <qxbthree@gmail.com>
2022-05-21 18:15:57 +0800

    feat(ready): added changing status behavior

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,5 +1,8 @@
 const { Listener } = require('@sapphire/framework')
 
+const presences = require('#assets/presences.json')
+const { Time } = require('@sapphire/time-utilities')
+
 class ReadyListener extends Listener {
   constructor(context) {
     super(context, {
@@ -8,8 +11,26 @@ class ReadyListener extends Listener {
   }
 
   run() {
-    const tag = this.container.client.user.tag
-    this.container.logger.info(`Logged in as ${tag}`)
+    const { client } = this.container
+    this.container.logger.info(`Logged in as ${client.user.tag}`)
+
+    this.setChangingStatus(client)
+  }
+
+  setChangingStatus(client) {
+    const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+    setInterval(() => {
+      const serverSize = client.guilds.cache.size
+      const userSize = client.guilds.cache.map((guild) => guild.memberCount).reduce((prev, current) => prev + current)
+
+      const presence = presences[randomNumber(0, presences.length-1)]
+      presence.name = presence.name
+        .replace(/{server_size}/g, `${serverSize}`)
+        .replace(/{user_size}/g, `${userSize}`)
+
+      client.user.setActivity(presence)
+    }, Time.Minute)
   }
 }
 
