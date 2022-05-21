9a412ca4b0c0a4ed377464ca8744fea4c171fd9d
qxb3 <qxbthree@gmail.com>
2022-05-21 17:50:39 +0800

    feat: added ping command

---
+++ src/commands/Bot/ping.js
@@ -0,0 +1,50 @@
+const MirageCommand = require('#structures/commands/MirageCommand')
+
+const { createEmbed, sendMessage } = require('#utils/response')
+const { getTestServer } = require('#utils/constants')
+
+class PingCommand extends MirageCommand {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      description: 'Check bot\'s latency',
+      chatInputCommand: {
+        register: true,
+        guildIds: [ getTestServer() ]
+      }
+    })
+  }
+
+  async run({ context }) {
+    const sent = await sendMessage(context, {
+      content: 'Pinging...',
+      reply: true,
+      fetchReply: true
+    })
+
+    const { diff, ping } = this.getPing(context, sent)
+
+    const embed = createEmbed()
+      .setTitle('Pong! 🏓')
+      .setDescription(
+        `⚡ | Roundtrip: ${diff}ms\n` +
+        `🏓 | Ping: ${ping}ms\n\n` +
+        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+      )
+      .setFooter({ text: 'Let\'s go!', iconURL: this.container.client.user.displayAvatarURL() })
+
+    await sent.edit({
+      content: ' ',
+      embeds: [embed]
+    })
+  }
+
+  getPing(context, sent) {
+    const diff = sent.createdTimestamp - context.createdTimestamp
+    const ping = Math.round(this.container.client.ws.ping)
+
+    return { diff, ping }
+  }
+}
+
+module.exports = PingCommand
