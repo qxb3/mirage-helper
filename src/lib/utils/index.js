4ea9b06b879b03ee33bf893ae0ead39965fda3b8
qxb3 <qxbthree@gmail.com>
2022-05-26 15:28:42 +0800

    feat(utils): added countLines func

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,3 +1,6 @@
+const { lstatSync, readdirSync, readFileSync } = require('fs')
+const { join } = require('path')
+
 /**
  * Generate random number
  * @param min {Number=0}
@@ -31,7 +34,45 @@ const formatTime = (time) => {
   return text.join(', ')
 }
 
+/**
+ * @typedef CountLines {Object}
+ * @property linesOfCode {Number}
+ * @property numOfFiles {Number}
+ *
+ * Count total line of code and files
+ * @param path {String}
+ * @returns {CountLines}
+ */
+const countlines = (path) => {
+  let linesOfCode = 0
+  let numOfFiles = 0
+
+  const lines = (dir) => {
+    const files = readdirSync(dir)
+
+    for (const file of files) {
+      const stat = lstatSync(join(dir, file))
+      if (stat.isDirectory()) {
+        lines(join(dir, file))
+      } else if (file.endsWith('.ts') || file.endsWith('.js') || file.endsWith('.d.ts') || file.endsWith('.js.map')) {
+        const buffer = readFileSync(join(dir, file)).toString()
+        const lines = buffer.split('\n')
+        linesOfCode += lines.length
+        numOfFiles++
+      }
+    }
+  }
+
+  if (linesOfCode === 0) lines(join(process.cwd(), path))
+
+  return {
+    linesOfCode,
+    numOfFiles
+  }
+}
+
 module.exports = {
   randomNumber,
-  formatTime
+  formatTime,
+  countlines
 }
