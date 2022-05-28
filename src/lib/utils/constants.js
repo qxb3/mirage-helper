1776cea0b2af1f94354172e8897e0959fe15c803
qxb3 <qxbthree@gmail.com>
2022-05-28 16:16:43 +0800

    feat(constants): addef success color

--- src/lib/utils/constants.js
+++ src/lib/utils/constants.js
@@ -3,6 +3,7 @@ const { Constants } = require('discord.js')
 const Colors = {
   Primary: Constants.Colors.DARK_BUT_NOT_BLACK,
   Secondary: Constants.Colors.ORANGE,
+  Success: Constants.Colors.GREEN,
   Error: Constants.Colors.RED
 }
 
