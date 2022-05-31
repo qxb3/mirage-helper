f3fb770cb73d1a8a9d83ca567456aec3b66dde9b
qxb3 <qxbthree@gmail.com>
2022-05-31 18:32:56 +0800

    refactor: renaming devSettings to just settings
    i forgot how this works mb

--- src/lib/models/dev.js
+++ src/lib/models/dev.js
@@ -3,7 +3,7 @@ const { requiredType } = require('./utils')
 
 const schema = new Schema({
   name: requiredType(String),
-  releaseSettings: requiredType(Object)
+  settings: requiredType(Object)
 })
 
 module.exports = model('dev_settings', schema)
